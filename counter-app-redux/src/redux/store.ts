cdd050be721f4d8c893be6e7d6325cfae416b45d
PriyaNayak20 <priya.n@webkorps.com>
2025-03-10 22:08:20 +0530

    update store file

--- counter-app-redux/src/redux/store.ts
+++ counter-app-redux/src/redux/store.ts
@@ -1,6 +1,14 @@
 import { createStore } from 'redux'
 import counterReducer from './counterReducer'
 
-const store = createStore(counterReducer)
+const persistedState = localStorage.getItem('counterState')
+  ? JSON.parse(localStorage.getItem('counterState')!)
+  : { count: 0 }
+
+const store = createStore(counterReducer, persistedState)
+
+store.subscribe(() => {
+  localStorage.setItem('counterState', JSON.stringify(store.getState()))
+})
 
 export default store
